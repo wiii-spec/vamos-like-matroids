b5e0c5ebf06b136f4f33cc4f09c738e5e305db1a
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2025-08-29 12:49:07 +0100

    bump to v4.6.0

--- lakefile.lean
+++ lakefile.lean
@@ -9,5 +9,4 @@ package «matroids» where
 lean_lib «Matroids» where
   -- add library configuration options here
 
-require std from git "https://github.com/leanprover/std4" @ s!"v{Lean.versionString}"
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"v{Lean.versionString}"
